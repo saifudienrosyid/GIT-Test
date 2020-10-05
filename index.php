2800c13968709ef264da633e220aadf4766a2d02
saifudienrosyid <saif.rosy16@gmail.com>
2020-10-05 10:43:55 +0700

    menambahkan list dibawah paragraf

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
 <body>
     <h1> Hello World </h1>
     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book<p>
+        <ul>
+            <li>satu<li>
+            <li>dua<li>
 </body>
 </html>
